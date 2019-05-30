84450a9e04fac246c289e870d9129f34399c2e36
Amit <amit0805kumar@gmail.com>
2019-05-30 21:48:08 +0530

    rellax js applied

--- js/script.js
+++ js/script.js
@@ -96,4 +96,30 @@ $(function () {
     setInterval(changeWord, 2000);
 
 
+    var rellax1 = new Rellax('.rellax1', {
+        speed: -3,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+    var rellax2 = new Rellax('.rellax2', {
+        speed: -2,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });    
+    var rellax3 = new Rellax('.rellax3', {
+        speed: 3,
+        center: false,
+        wrapper: null,
+        round: true,
+        vertical: true,
+        horizontal: false
+    });
+
+
 });
